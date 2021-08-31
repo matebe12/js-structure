b0cb9e115888f31fa59c528fe08fd852a16ddc8a
Lee Sang Yeon <42566975+matebe12@users.noreply.github.com>
2021-08-31 17:47:26 +0900

    intersection study

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,10 +16,11 @@
   <div class="item">
     Item 4
   </div>
-  <div class="scroll-detecting"></div>
+  <div class="scroll-detecting">
+    <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
   </div>
-  
-</div>
+  </div>
+  </div>
 </template>
 
 <script>
@@ -58,13 +59,16 @@ console.log('intersectionRatio : ',item.intersectionRatio);
   });
 
   if (entries.some(entry => entry.intersectionRatio > 0)) {
-    loadItem(10)
-    root.appendChild(detector)
+    setTimeout(function(){
+      loadItem(10)
+      root.appendChild(detector)
+
+    },1000)
   }
 },{
   root:document.querySelector('.root'),
   rootMargin: '0px',
-  threshold: 0.1
+  threshold: 0.8
 })
   io.observe(detector)
   }
@@ -89,5 +93,40 @@ console.log('intersectionRatio : ',item.intersectionRatio);
 .scroll-detecting {
   width: 5px;
   height: 5px;
+}
+.lds-ring {
+  display: inline-block;
+  position: relative;
+  width: 64px;
+  height: 64px;
+}
+.lds-ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 51px;
+  height: 51px;
+  margin: 6px;
+  border: 6px solid #fff;
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: #F86C06 transparent transparent transparent;
+}
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
